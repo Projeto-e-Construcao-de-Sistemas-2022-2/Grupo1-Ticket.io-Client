4b983f5fc300c647c1d8523498197b4e3cdfbbd9
Junior M. Soares <jrmsrs2.0@gmail.com>
2022-11-22 17:14:46 -0300

    Expose firebaseConfig.js

--- src/services/FirebaseConfig.js
+++ src/services/FirebaseConfig.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'AIzaSyAsYQq3aOmEZN-c72r0AH5UTTOdOq9RmYg',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'ticket-io-auth.firebaseapp.com',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'ticket-io-auth',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'ticket-io-auth.appspot.com',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "1033659714959",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || '1:1033659714959:web:05eeeb11ed3dcec2b78ffa'
 };
 
 export const app = initializeApp(firebaseConfig);
+
